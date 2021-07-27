548ac901bd5ca2fe38b106bf9ca224c46c499012
vasudhapuri <75962299+vasudhapuri@users.noreply.github.com>
2021-07-27 11:43:51 -0700

    Created pic controller api

---
+++ EventCatalogAPI/Controllers/PicController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PicController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+        public PicController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [Route("{id}")]
+        public IActionResult GetImage(int id)
+        {
+            var webroot = _env.WebRootPath;
+            var path = Path.Combine($"{webroot}/pics", $"Event{id}.jpg");
+            var buffer=System.IO.File.ReadAllBytes(path);
+            return File(buffer, "image/jpeg");
+
+        }
+    }
+}
