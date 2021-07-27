370255351ff1e6dc40f8fa327b61ef1166ddf666
vasudhapuri <75962299+vasudhapuri@users.noreply.github.com>
2021-07-27 09:21:22 -0700

    Removed extra cities, added data to events table

--- EventCatalogAPI/Data/EventSeed.cs
+++ EventCatalogAPI/Data/EventSeed.cs
@@ -44,36 +44,37 @@ namespace EventCatalogAPI.Data
                 new EventLocation
                 {
                     City="Bellevue"
-                },
-                new EventLocation
-                {
-                    City="Renton"
-                },
-                new EventLocation
-                {
-                    City="Seattle"
-                },
-                 new EventLocation
-                {
-                    City="Bothell"
-                },
-                  new EventLocation
-                {
-                    City="Kenmore"
-                },
-                   new EventLocation
-                {
-                    City="Sammamish"
-                },
-                    new EventLocation
-                {
-                    City="Issaquah"
-                },
-                      new EventLocation
-                {
-                    City="Woodinville"
-                      }
+                }
                 };
+                //new EventLocation
+                //{
+                //    City="Renton"
+                //},
+                //new EventLocation
+                //{
+                //    City="Seattle"
+                //},
+                // new EventLocation
+                //{
+                //    City="Bothell"
+                //},
+                  //new EventLocation
+                //{
+                //    City="Kenmore"
+                //},
+                //   new EventLocation
+                //{
+                //    City="Sammamish"
+                //},
+                //    new EventLocation
+                //{
+                //    City="Issaquah"
+                //},
+                //      new EventLocation
+                //{
+                //    City="Woodinville"
+                //      }
+                //};
 
         }
         public static IEnumerable<EventType> GetEventTypes()
@@ -107,12 +108,8 @@ namespace EventCatalogAPI.Data
                   new EventType
                 {
                     TypeName="Online"
-                },
-                   new EventType
-                {
-                    TypeName="Free"
                 }
-
+                  
             };
 
         }
@@ -122,21 +119,26 @@ namespace EventCatalogAPI.Data
             {
 
                 new EachEvent { TypeId = 4, LocationId = 1, Description = "Try it, taste it, adopt it", EventName = "Be A Vegan:Beginner", Price = 0.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/1", Address="Araya's vegan cafe hut", Likes=10, Date=new DateTime(2021, 09, 09, 10, 10, 00 ), Zip="98034", TicketType="Free" },
-                new EachEvent { TypeId = 1, LocationId = 2, Description = "Get high at Trinity Nightclub", EventName = "SunTan Disco", Price = 35.99M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/2", Address="Rose hill hall",Likes=2,  Date=new DateTime(2021, 09, 11, 21, 00, 00 ) ,Zip="98033" ,TicketType="Paid" },
+                new EachEvent { TypeId = 1, LocationId = 2, Description = "Get high at Trinity Nightclub", EventName = "SunTan Disco", Price = 35.99M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/2", Address="Rose hill hall,124th ave, Kirkland",Likes=2,  Date=new DateTime(2021, 09, 11, 21, 00, 00 ) ,Zip="98055" ,TicketType="Paid" },
                 new EachEvent { TypeId = 3, LocationId = 3, Description = "You have already won gold medal", EventName = "Pinkathon", Price = 10.50M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/3", Address="Bellevue stadium",Likes=0,  Date=new DateTime(2021, 09, 05, 10, 30, 00 ), Zip="98009",TicketType="Paid"},
-                new EachEvent { TypeId = 3, LocationId = 10, Description = "Dine for a cause", EventName = "Foundation Hitech Fundraiser", Price = 12.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/4", Address="Boys and girls club,84th street", Date=new DateTime(2021, 09, 06, 10, 10, 00 ), Zip="98033" ,TicketType="Paid"},
-                new EachEvent { TypeId = 2, LocationId = 5, Description = "Wine, Yoga and Fun", EventName = "Heritage Brunch", Price = 40.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/5" , Address="Novotel, Seattle",Likes=7,  Date=new DateTime(2021, 09, 22, 12, 30, 00 ), Zip="98016", TicketType="Paid"},
-                new EachEvent { TypeId = 7, LocationId = 6, Description = "Get placed at your dream job", EventName = "Primier Virtual Tech", Price = 50.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/6", Address="Remote" ,Likes =2,  Date=new DateTime(2021, 08, 22, 11, 30, 00 ), Zip="98011", TicketType="Trial"},
-                new EachEvent { TypeId = 2, LocationId = 7, Description = "Saturday brunch with friends and family!", EventName = "Eat All You Can", Price = 50.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/7" , Address="Sea high, golf street, Kenmore", Likes=3,  Date=new DateTime(2021, 09, 09, 13, 00, 00 ), Zip="99027" ,TicketType="Paid"},
-                new EachEvent { TypeId = 4, LocationId = 10, Description = "Powerpacked Yoga, meditation and Zumba", EventName = "Summer Fitness Expo", Price = 15.99M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/8", Address="Brit's dance academy",  Likes=9,  Date=new DateTime(2021, 08, 25, 11, 00, 00 ) , Zip="98816", TicketType="Subsrciption"},
-                new EachEvent { TypeId = 5, LocationId = 2, Description = "Get caught, get lost in the workd of Shakespeare", EventName = "Shakespeare", Price = 10.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/9", Address="Great Lake stadium, Kirkland", Likes=8, Date=new DateTime(2021, 09, 13, 20, 15, 00 ) , Zip="98033", TicketType="Paid"},
-                new EachEvent { TypeId = 2, LocationId = 4, Description = "Achieve your culinary dreams", EventName = "Bakethon2.0", Price = 12.50M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/10" , Address="Sasha's palace kitchen", Likes=1,  Date=new DateTime(2021, 09, 09, 17, 00, 00 ) , Zip="98331", TicketType="Trial"},
-                new EachEvent { TypeId = 6, LocationId = 8, Description = "Be an all rounder, get hired at the best!", EventName = "Hitech Job Expo", Price = 25.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/11" , Address="Sea hawks stadium, Sammamish", Likes=0,  Date=new DateTime(2021, 08, 10, 10, 10, 00 ) , Zip="98074" ,TicketType="Subscription"},
-                new EachEvent { TypeId = 7, LocationId = 1, Description = "A pricesless vegan experience", EventName = "CookVegan", Price = 20.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/12", Address="Remote",  Likes=3,Date=new DateTime(2021, 08, 25, 09, 30, 00 ), Zip="98008",TicketType="Paid" },
-                new EachEvent { TypeId = 5, LocationId = 3, Description = "The most loved and breathtaking play of the year", EventName = "Elequent", Price = 30.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/13" , Address="Grandeur indoor Park", Likes=5,  Date=new DateTime(2021, 09, 30, 19, 00, 00 ), Zip="98008", TicketType="Paid" },
-                new EachEvent { TypeId = 1, LocationId = 9, Description = "Massive Fridays with DJ Samuel", EventName = "Summer Star Electric", Price = 70.99M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/14", Address="Q Nighclub", Likes=11,  Date=new DateTime(2021, 08, 30, 21, 30, 00 ), Zip="98029", TicketType="Paid" },
-                new EachEvent { TypeId = 8, LocationId = 1, Description = "Come.. take part, start the change", EventName = "Peace Youth Peloton", Price = 0.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/15", Address="Redmond high school, 46th Ave",Likes=9,  Date=new DateTime(2021, 09, 14, 07, 30, 00 ), Zip="98053", TicketType="Free" }
-                };
+                new EachEvent { TypeId = 3, LocationId = 2, Description = "Dine for a cause", EventName = "Foundation Hitech Fundraiser", Price = 12.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/4", Address="Boys and girls club,84th street", Date=new DateTime(2021, 09, 06, 10, 10, 00 ), Zip="98055" ,TicketType="Paid"},
+                new EachEvent { TypeId = 2, LocationId = 2, Description = "Wine, Yoga and Fun", EventName = "Heritage Brunch", Price = 40.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/5" , Address="Novotel, Kirkland",Likes=7,  Date=new DateTime(2021, 09, 22, 12, 30, 00 ), Zip="98055", TicketType="Paid"},
+                new EachEvent { TypeId = 7, LocationId = 1, Description = "Get placed at your dream job", EventName = "Primier Virtual Tech", Price = 50.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/6", Address="Remote" ,Likes =2,  Date=new DateTime(2021, 08, 22, 11, 30, 00 ), Zip="98033", TicketType="Trial"},
+                new EachEvent { TypeId = 2, LocationId = 2, Description = "Saturday brunch with friends and family!", EventName = "Eat All You Can", Price = 50.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/7" , Address="Sea high, golf street 11th ave", Likes=3,  Date=new DateTime(2021, 09, 09, 13, 00, 00 ), Zip="99054" ,TicketType="Paid"},
+                new EachEvent { TypeId = 4, LocationId = 3, Description = "Powerpacked Yoga, meditation and Zumba", EventName = "Summer Fitness Expo", Price = 15.99M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/8", Address="Brit's dance academy",  Likes=9,  Date=new DateTime(2021, 08, 25, 11, 00, 00 ) , Zip="98810", TicketType="Subsrciption"},
+                new EachEvent { TypeId = 5, LocationId = 1, Description = "Get caught, get lost in the workd of Shakespeare", EventName = "Shakespeare", Price = 10.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/9", Address="Great Lake stadium", Likes=8, Date=new DateTime(2021, 09, 13, 20, 15, 00 ) , Zip="98033", TicketType="Paid"},
+                new EachEvent { TypeId = 2, LocationId = 3, Description = "Achieve your culinary dreams", EventName = "Bakethon2.0", Price = 12.50M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/10" , Address="Sasha's palace kitchen", Likes=1,  Date=new DateTime(2021, 09, 09, 17, 00, 00 ) , Zip="98810", TicketType="Trial"},
+                new EachEvent { TypeId = 6, LocationId = 3, Description = "Be an all rounder, get hired at the best!", EventName = "Hitech Job Expo", Price = 25.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/11" , Address="Sea hawks stadium", Likes=0,  Date=new DateTime(2021, 08, 10, 10, 10, 00 ) , Zip="98810" ,TicketType="Subscription"},
+                new EachEvent { TypeId = 7, LocationId = 2, Description = "A pricesless vegan experience", EventName = "CookVegan", Price = 20.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/12", Address="Remote",  Likes=3,Date=new DateTime(2021, 08, 25, 09, 30, 00 ), Zip="98056",TicketType="Paid" },
+                new EachEvent { TypeId = 5, LocationId = 3, Description = "The most loved and breathtaking play of the year", EventName = "Elequent", Price = 30.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/13" , Address="Grandeur indoor Park", Likes=5,  Date=new DateTime(2021, 09, 30, 19, 00, 00 ), Zip="98009", TicketType="Paid" },
+                new EachEvent { TypeId = 1, LocationId = 2, Description = "Massive Fridays with DJ Samuel", EventName = "Summer Star Electric", Price = 70.99M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/14", Address="Q Nighclub", Likes=11,  Date=new DateTime(2021, 08, 30, 21, 30, 00 ), Zip="98029", TicketType="Paid" },
+                new EachEvent { TypeId = 3, LocationId = 1, Description = "Come.. take part, start the change", EventName = "Peace Youth Peloton", Price = 0.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/15", Address="Redmond high school, 46th Ave",Likes=9,  Date=new DateTime(2021, 09, 14, 07, 30, 00 ), Zip="98034", TicketType="Free" },                
+                new EachEvent { TypeId = 4, LocationId = 3, Description = "Join for a fun vinyasa yoga flow to music while enjoying the sunshine and the lake view", EventName = "Vinyasa Yoga in the Park", Price = 20.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/16" , Address="Bellevue downtown park", Likes=0,  Date=new DateTime(2021, 08, 10, 10, 10, 00 ) , Zip="98009" ,TicketType="Subscription"},
+                new EachEvent { TypeId = 1, LocationId = 1, Description = "An open-format DJ specializing in mixing pop, country, hip-hop and rock", EventName ="On the rock music" , Price = 79.99M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/17", Address="Queer Bar, 11th ave",  Likes=1,Date=new DateTime(2021, 08, 25, 09, 30, 00 ), Zip="98033",TicketType="Paid" },
+                new EachEvent { TypeId = 4, LocationId = 1, Description = "Hike through meadows, along mountain streams, and over rocky passes.", EventName = "Summer teen hike", Price = 25.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/18" , Address="Redmond ridge trails", Likes=5,  Date=new DateTime(2021, 09, 30, 19, 00, 00 ), Zip="98033", TicketType="Paid" },
+                new EachEvent { TypeId = 1, LocationId = 2, Description = "Indie/Acoustic/Folk act from Cleveland, Ohio", EventName = "DJ Tony", Price = 89.99M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/19", Address="Green club, Kirkland", Likes=10,  Date=new DateTime(2021, 08, 30, 21, 30, 00 ), Zip="98056", TicketType="Paid" },
+                new EachEvent { TypeId = 6, LocationId = 1, Description = "Business conference that helps you become a stronger leader", EventName = "Seattle yearly career seminar", Price = 0.00M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/20", Address="Redmond high school, 46th Ave",Likes=9,  Date=new DateTime(2021, 09, 14, 07, 30, 00 ), Zip="98034", TicketType="Free" },
+            };
         }
 
     }
